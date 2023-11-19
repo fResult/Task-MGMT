ff14521382debf7d10b912d1c82438345356bb15
fResult Sila <Styxmaz@gmail.com>
2023-11-19 13:01:06 +0700

    Keep generic copy

--- src/main/kotlin/dev/fresult/taskmgmt/services/TaskService.kt
+++ src/main/kotlin/dev/fresult/taskmgmt/services/TaskService.kt
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.LocalDate
+import kotlin.reflect.KProperty1
 
 @Service
 class TaskService(private val repository: TaskRepository) : BaseService<Task, Long> {
@@ -28,43 +29,52 @@ class TaskService(private val repository: TaskRepository) : BaseService<Task, Lo
     repository.save(task)
   }
 
-override fun deleteById(id: Long): Mono<Void> {
-  return repository.deleteById(id)
-}
+  override fun deleteById(id: Long): Mono<Void> {
+    return repository.deleteById(id)
+  }
 
-fun allByQueryParams(queryParams: TaskQueryParamValues): Flux<Task> {
-  val (dueDates, statuses, createdBy, updatedBy) = queryParams
+  fun allByQueryParams(queryParams: TaskQueryParamValues): Flux<Task> {
+    val (dueDates, statuses, createdBy, updatedBy) = queryParams
 
-  return repository.findAllByConditions(
-    defaultFirstLong(createdBy),
-    defaultFirstLong(updatedBy),
-    defaultFirstItem(::firstItemLocalDate)(dueDates),
-    defaultFirstItem(::firstItemStatus)(statuses),
-    createdByIsNull = createdBy.isNullOrEmpty(),
-    updatedByIsNull = updatedBy.isNullOrEmpty(),
-    dueDatesIsNull = dueDates.isNullOrEmpty(),
-    statusesIsNull = statuses.isNullOrEmpty(),
-  )
-}
-
-val copyToUpdate: (Task) -> (Task) -> Task = { existingTask ->
-  { task ->
-    task.copy(
-      id = existingTask.id,
-      version = existingTask.version,
-      createdAt = existingTask.createdAt,
-      updatedAt = existingTask.updatedAt,
-      createdBy = existingTask.createdBy
+    return repository.findAllByConditions(
+      defaultFirstLong(createdBy),
+      defaultFirstLong(updatedBy),
+      defaultFirstItem(::firstItemLocalDate)(dueDates),
+      defaultFirstItem(::firstItemStatus)(statuses),
+      createdByIsNull = createdBy.isNullOrEmpty(),
+      updatedByIsNull = updatedBy.isNullOrEmpty(),
+      dueDatesIsNull = dueDates.isNullOrEmpty(),
+      statusesIsNull = statuses.isNullOrEmpty(),
     )
   }
-}
 
-private fun <T> defaultFirstItem(default: () -> List<T>): (List<T>?) -> List<T> = { list ->
-  list.orEmpty().ifEmpty(default)
-}
+  val copyToUpdate: (Task) -> (Task) -> Task = { existingTask ->
+    { task ->
+      task.copy(
+        id = existingTask.id,
+        version = existingTask.version,
+        createdAt = existingTask.createdAt,
+        updatedAt = existingTask.updatedAt,
+        createdBy = existingTask.createdBy
+      )
+    }
+  }
 
-private fun firstItemLong(): List<Long> = listOf(-1)
-private val defaultFirstLong = defaultFirstItem(::firstItemLong)
-private fun firstItemLocalDate(): List<LocalDate> = listOf(LocalDate.now())
-private fun firstItemStatus(): List<TaskStatus> = listOf(TaskStatus.PENDING)
+  fun <T : Any> copy2(vararg props: KProperty1<T, Any?>): (T) -> (T) -> T {
+    return { existedItem ->
+      { originalItem ->
+        val copiedSomeProps = props.associateWith { it.get(existedItem) }
+        originalItem.copy(copiedSomeProps)
+      }
+    }
+  }
+
+  private fun <T> defaultFirstItem(default: () -> List<T>): (List<T>?) -> List<T> = { list ->
+    list.orEmpty().ifEmpty(default)
+  }
+
+  private fun firstItemLong(): List<Long> = listOf(-1)
+  private val defaultFirstLong = defaultFirstItem(::firstItemLong)
+  private fun firstItemLocalDate(): List<LocalDate> = listOf(LocalDate.now())
+  private fun firstItemStatus(): List<TaskStatus> = listOf(TaskStatus.PENDING)
 }
