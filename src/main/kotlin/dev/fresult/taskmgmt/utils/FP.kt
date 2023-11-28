400a2ef32c1462eabd0eaedbd6e0add2f53a4a65
fResult Sila <Styxmaz@gmail.com>
2023-11-28 16:28:44 +0700

    Add JavaDoc for `then` infix function

--- src/main/kotlin/dev/fresult/taskmgmt/utils/FP.kt
+++ src/main/kotlin/dev/fresult/taskmgmt/utils/FP.kt
@@ -1,5 +1,24 @@
 package dev.fresult.taskmgmt.utils
 
-infix fun <A1, A2, R> ((A1) -> A2).then(other: (A2) -> R): (A1) -> R {
-  return { other(this(it)) }
+/**
+ * Use like `compose`, but reversed
+ * ```txt
+ * g(f(x)) ≡ (g ∘ f)(x)
+ *   f     :: A → B
+ *   g     :: B → C
+ * ∴ g ∘ f :: A → C
+ * (g ∘ f pronounced "g compose f" OR "g after f")
+ * ```
+ * **Usage:**
+ * ```kt
+ * val f: (Int) -> Int = { x -> x + 10 }
+ * val g: (Int) -> Int = { x -> x + 2 }
+ * val x = 5
+ * val composedFunc = f then g
+ * println(composedFunc(x)) // 17
+ * ```
+ */
+infix fun <A1, A2, R> ((A1) -> A2).then(g: (A2) -> R): (A1) -> R = { x ->
+  val f = this
+  g(f(x))
 }
